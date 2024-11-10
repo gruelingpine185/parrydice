5fc89c0ed87cb3107e04d71ad117c1f54252979b
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-11-10 12:17:08 -0500

    Create expect.h

---
+++ src/utils/expect.h
@@ -0,0 +1,46 @@
+#ifndef PD_ASSERT_H
+#define PD_ASSERT_H
+
+#include "utils/defines.h"
+#include "utils/types.h"
+
+typedef enum {
+    PD_EXPECT_TYPE_PTR,
+    PD_EXPECT_TYPE_EXPR
+} pd_expect_type;
+
+typedef enum {
+    PD_EXPECT_OP_EQ,
+    PD_EXPECT_OP_NE,
+    PD_EXPECT_OP_LT,
+    PD_EXPECT_OP_GT,
+    PD_EXPECT_OP_LTE,
+    PD_ASSRET_OP_GTE
+} pd_expect_op;
+
+typedef struct pd_expect_ctx pd_expect_ctx;
+typedef union pd_expect_value pd_expect_value;
+
+union pd_expect_value {
+    struct {
+        void* l;
+        void* r;
+    } as_ptr;
+
+    struct {
+        i32 l;
+        i32 r;
+    } as_expr;
+};
+
+struct pd_expect_ctx {
+    pd_expect_type type;
+    pd_expect_op op;
+    const char* l_as_str;
+    const char* r_as_str;
+    pd_expect_value value;
+};
+
+
+#endif //SERT_H
+
