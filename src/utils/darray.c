f3c6522b40bc4d561f3064c69de07736a435fe23
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-11-11 19:11:46 -0500

    resize

--- src/utils/darray.c
+++ src/utils/darray.c
@@ -3,6 +3,20 @@
 #include "utils/expect.h"
 #include "utils/darray.h"
 
+static b32 pd_darray_resize(pd_darray* _arr);
+
+
+static b32 pd_darray_resize(pd_darray* _arr) {
+    const f32 resize_factor = 1.61f;
+    const usize new_cap = pd_darray_r_cap(_arr) * resize_factor;
+    void** tmp = (void**) realloc(&_arr->data, (sizeof(void*) * new_cap));
+    if(!tmp) return 0;
+
+    _arr->data = tmp;
+    _arr->cap = new_cap;
+    return 1;
+}
+
 b32 pd_darray_init(pd_darray* _arr) {
     PD_expect_nonnull(_arr);
 
